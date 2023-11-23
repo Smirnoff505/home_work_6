cfb7e6c98d5b742b8caf7289fe43ff954ed2a2da
Oleg Smirnov <oleg.smirnoff505@yandex.ru>
2023-11-24 02:22:45 +0500

    Add files

---
+++ catalog/tamplatetags/my_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag()
+@register.filter()
+def mediapath(val):
+    if val:
+        return f'/media/{val}'
+
+    return '#'
