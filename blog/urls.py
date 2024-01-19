ad18692da79ca44ba85f4a203bbd94457e4df205
Oleg Smirnov <oleg.smirnoff505@yandex.ru>
2024-01-20 00:44:53 +0500

    changes files

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from blog.apps import BlogConfig
 from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView, toggle_activity
@@ -6,10 +7,10 @@ from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateV
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('create/', BlogCreateView.as_view(), name='create'),
+    path('create/', never_cache(BlogCreateView.as_view()), name='create'),
     path('list/', BlogListView.as_view(), name='list'),
     path('detail/<str:slug>/', BlogDetailView.as_view(), name='detail'),
-    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit'),
+    path('edit/<int:pk>/', never_cache(BlogUpdateView.as_view()), name='edit'),
     path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
     path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
 ]
